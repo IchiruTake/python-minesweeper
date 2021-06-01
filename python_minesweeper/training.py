63b2606490f841d15e5a1815e668afeaf245912a
Ichiru Take <66363520+IchiruTake@users.noreply.github.com>
2021-06-01 08:55:47 +0700

    Update training.py

--- python_minesweeper/training.py
+++ python_minesweeper/training.py
@@ -1,8 +1,13 @@
 from deep_learning import LearningModel
 
-if __name__ == "__main__":
-    Learn = LearningModel(folder="model/", matrixSize=8, numberOfSamples=int(2e6), sigmoidLearning=True,
+
+def train(samples: int = int(2e6), verbose: int = 10000):
+    Learn = LearningModel(folder="model/", matrixSize=8, numberOfSamples=samples, sigmoidLearning=True,
                           tanhLearning=False, easyRatio=0.7, mediumRatio=0.15, hardRatio=0.1, extremeRatio=0.05)
-    Learn.build(minimum_hashing=False, verbose=10000)
+    Learn.build(minimum_hashing=False, verbose=verbose)
     Learn.prepare(test_size=0.05)
     Learn.train(trainPath="model/Train Prediction.csv", testPath="model/Test Prediction.csv")
+
+
+if __name__ == "__main__":
+    train(samples=100000, verbose=5000)
